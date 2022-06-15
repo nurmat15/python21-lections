4b2ce45927280945a4862c1eb95842dfdbec2acb
nurmat15 <nur150488mat@gmail.com>
2022-06-15 12:38:31 +0600

    loops

--- basics/dictionaries.py
+++ basics/dictionaries.py
@@ -19,6 +19,26 @@ print(dict_[4.5]) # {"a":5}
 print(dict_[4.5]["a"]) # 5
 print{dict_["a"]} # KeyError: "a"
 
+"=============Создание словарей================"
+dict1 = {"a":3}
+dict2 = dict( [ ('key1', 'value1'), ('key2', 'value2') ] )
+# dict2 = {'key1':'value1', 'key2':'value2'}
+dict3 = dict( ( ['key1', 'value1'], ('key2', 'value2') ) )
+# dict3 = {'key1':'value1', 'key2':'value2'}
+dict4 = dict(['ab', 'cd', 'de'])
+# dict4 = {"a":"b", 'c':'d', 'd':'e'}
+key1, value1 = 'ab'
+dict4[key1] = value1
+key2, value2 = 'cd'
+dict4[key2] = value2
+key3, value3 = 'de'
+dict4[key3] = value3
+
+
+dict5 = dict(['abc']) # ValueError: dictionary update sequence element #0 has length 3; 2 is required
+key1, value1 = 'abc' #
+dict5[key1] = value1
+
 "=======Metody slovarya========"
 dict_.clear() # 4istit slovar
     print(dict_) # {}
